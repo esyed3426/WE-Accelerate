f63a7519706a4d5290a589a53d45e8f4be87093e
Ehsan Syed <89548236+esyed3426@users.noreply.github.com>
2022-06-01 14:32:24 +0100

    updated

--- Modern JavaScript/notes.js
+++ Modern JavaScript/notes.js
@@ -3,3 +3,48 @@ const sumTwo = (n1, n2) => n1 + n2
 function sumTwo(n1, n2) {
     return n1 + n2;
 }
+
+
+// Examples of closure: 
+
+function createCounter() {
+    let counter = 0;
+    return function() {
+        counter = counter + 1;
+        return counter;
+        }
+    }
+
+const increment = createCounter();
+increment(); // "1"
+increment(); // "2"
+
+
+function createMultiplier(n1) {
+    return function multiply(n2) {
+        return n1 * n2;
+    }
+}
+
+const multiplyByTwo = createMultiplier(2);
+multiplyByTwo(3); // "6"
+multiplyByTwo(4); // "8"
+
+// End of closures
+
+// Callbacks:
+
+function callApi(url, callback) {
+    // some code to fetch data from {url}
+    callback(data);
+}
+
+function onSuccess(data) {
+    // do something with data
+}
+    
+callApi('http://some-api/v1', onSuccess);
+
+// End of callbacks
+    
+    
