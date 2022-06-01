37c03f47b7627390750ea7ee2ec614fabaf5680f
Ehsan Syed <89548236+esyed3426@users.noreply.github.com>
2022-06-01 16:23:25 +0100

    updates

---
+++ Modern JavaScript/esyed3426-modern-js/src/index2.js
@@ -0,0 +1,15 @@
+// Functions
+
+function greet(msg = 'Hello world') {
+    // code goes here
+    console.log(msg);
+}
+
+greet();
+
+let myFunc = (msg) => {
+    console.log(msg);
+};
+
+myFunc('hello');
+
