ac2f6fa16599f9deac0aa33c5cdd675e3fd257df
Ehsan Syed <89548236+esyed3426@users.noreply.github.com>
2022-06-03 15:29:27 +0100

    added some things

---
+++ REST/example/index.js
@@ -0,0 +1,24 @@
+import express from "express";
+const app = express();
+const { PORT = 3000 } = process.env;
+import bodyParser from "body-parser";
+import cors from "cors";
+app.use(bodyParser.json()).use(cors());
+app.get("/", (request, response) => response.send("Hello World"));
+app.get("/healthcheck", (req, res) => {
+    let healthcheck = {
+        uptime: process.uptime(),
+        message: "OK",
+        timestamp: Date.now(),
+    };
+    try {
+        res.send(healthcheck);
+    } catch (error) {
+        healthcheck.message = error;
+        res.status(503).send(healthcheck);
+    }
+});
+app.listen(PORT, () =>
+    console.log(`Hello World, I'm listening on http://localhost:${PORT}/`)
+);
+export default app;
